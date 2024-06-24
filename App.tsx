62645608f3f1bdd0d2f925226a25d5d234151582
anjalikumari2207 <150237165+anjalikumari2207@users.noreply.github.com>
2024-06-24 15:33:05 +0530

    EmailVerificScreen Added

--- App.tsx
+++ App.tsx
@@ -26,7 +26,6 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import TabNavigation from './src/UI/Components/TabNavigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import EmailVerificationScreen from './src/UI/Screens/EmailVerificationScreen';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -73,7 +72,30 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-     <EmailVerificationScreen/>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
