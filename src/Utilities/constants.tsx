cff616d27bba9b4421b6615468a2001137e3a2de
Abhinav Anand <anandabhinav785@gmail.com>
2024-06-25 23:14:01 +0530

    done changes in styles.tsx file, created constants file to include similar measurements for different screens , updated the well formatted code , removed comments from code, removed unnecessary imports and removed white lines from app.json folder

---
+++ src/Utilities/constants.tsx
@@ -0,0 +1,44 @@
+export const DIMENSIONS = {
+    cameraWidth: 200,
+    cameraHeight: 300,
+    cameraViewWidth: 200,
+    cameraViewHeight: 330,
+    buttonWidth: 250,
+    buttonHeight: 28,
+    dotSize: 15,
+  };
+  
+  export const PADDING = {
+    small: 8,
+    medium: 10,
+    large: 20,
+  };
+  
+  export const MARGIN = {
+    small: 5,
+    medium: 10,
+    large: 15,
+    extraLarge: 30,
+  };
+  
+  export const FONT_SIZES = {
+    small: 15,
+    medium: 18,
+    large: 25,
+  };
+  
+  export const BORDER_RADIUS = {
+    small: 5,
+    medium: 100,
+    large: Math.max(200, 330) / 2,
+  };
+  
+  export const COLORS = {
+    primary: '#c45435',
+    secondary: '#333',
+    background: 'transparent',
+    overlayBackground: '#fafce0',
+    white: '#ffffff',
+    black: '#000000',
+  };
+  
