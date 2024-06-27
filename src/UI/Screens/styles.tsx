ba76c2f3243e36cd3dfb0289dfb5a4cb79216b8d
Abhinav Anand <anandabhinav785@gmail.com>
2024-06-27 17:22:17 +0530

    fixed bugs in visualElementscreen file, made styles file seperately and tested it

---
+++ src/UI/Screens/styles.tsx
@@ -0,0 +1,34 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F7F7F7',
+    marginBottom: 20,
+  },
+  imageContainer: {
+    width: '100%',
+    height: '50%',
+    borderRadius: 20, //
+    overflow: 'hidden',
+    marginBottom: 30,
+  },
+  image: {
+    width: 350,
+    height: 300,
+  },
+  lottieContainer: {
+    width: '100%',
+    height: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottie: {
+    width: 350,
+    height: 300,
+  },
+});
+
+export default styles;
