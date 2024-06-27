fcd201437d51c92505bd8aff7c18c53378c4d265
Abhinav Anand <anandabhinav785@gmail.com>
2024-06-27 17:59:54 +0530

    taken latest pull from develop and updated the code

--- src/Utilities/constants.tsx
+++ src/Utilities/constants.tsx
@@ -1,44 +1,48 @@
+// src/utilities/constants.tsx
 export const DIMENSIONS = {
-    cameraWidth: 200,
-    cameraHeight: 300,
-    cameraViewWidth: 200,
-    cameraViewHeight: 330,
-    buttonWidth: 250,
-    buttonHeight: 28,
-    dotSize: 15,
-  };
-  
-  export const PADDING = {
-    small: 8,
-    medium: 10,
-    large: 20,
-  };
-  
-  export const MARGIN = {
-    small: 5,
-    medium: 10,
-    large: 15,
-    extraLarge: 30,
-  };
-  
-  export const FONT_SIZES = {
-    small: 15,
-    medium: 18,
-    large: 25,
-  };
-  
-  export const BORDER_RADIUS = {
-    small: 5,
-    medium: 100,
-    large: Math.max(200, 330) / 2,
-  };
-  
-  export const COLORS = {
-    primary: '#c45435',
-    secondary: '#333',
-    background: 'transparent',
-    overlayBackground: '#fafce0',
-    white: '#ffffff',
-    black: '#000000',
-  };
-  
+  cameraWidth: 200,
+  cameraHeight: 300,
+  cameraViewWidth: 200,
+  cameraViewHeight: 330,
+  dotSize: 15,
+  buttonWidth: 250,
+  buttonHeight: 28,
+};
+
+export const COLORS = {
+  background: 'transparent',
+  cameraBackground: 'black',
+  overlayBackground: '#fafce0',
+  textColor: '#333',
+  buttonBackground: '#c45435',
+  buttonText: 'white',
+};
+
+export const BORDER_RADIUS = {
+  camera: 100,
+  cameraView: Math.max(200, 330) / 2,
+  button: 5,
+};
+
+export const PADDING = {
+  cameraView: 10,
+  overlayVertical: 20,
+  overlayTop: 20,
+};
+
+export const MARGIN = {
+  cameraViewTop: 30,
+  cameraViewBottom: 45,
+  textBottom: 10,
+  dotBottom: 8,
+  dotRight: 5,
+  codeBottom: 15,
+  buttonBottom: 10,
+  buttonTextTop: 3,
+};
+
+export const FONT_SIZES = {
+  text: 18,
+  code: 25,
+  buttonText: 15,
+};
