0518914d55ea933ddc40fe14b3bbd54338d7be08
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-06-27 09:00:42 +0530

    Deleted Contants tsx files

--- src/Constants/PlaceHolders.tsx
+++
@@ -1,6 +0,0 @@
-
-export const EMAIL_VERIFICATION_TITLE = "Select your personal email ID";
-export const EMAIL_VERIFICATION_SUBTITLE = "This email ID will be linked to your account";
-export const EMAIL_ID_PLACEHOLDER = "Select your email ID";
-export const EMAIL_PLACEHOLDER = "Enter your email ID";
-export const NAME_PLACEHOLDER = "Enter your name";
--- src/Constants/TimeDateFormat.tsx
+++
@@ -1,3 +0,0 @@
-const DATE_FORMAT = 'DD/MM/YYYY';
-const TIME_FORMAT = 'HH:mm:ss';
-const DATETIME_FORMAT = 'DD/MM/YYYY HH:mm:ss';
