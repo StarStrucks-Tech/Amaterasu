4c0b6dafb5586c3ef4ab7f53159917fe32385187
Abhinav Anand <anandabhinav785@gmail.com>
2024-06-27 16:18:25 +0530

    fixed bugs in visualElement.tsx file , made styles folder and completed testing

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
+// App.tsx
 import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import LivelinessScreen from './src/UI/Screens/Liveliness screens/LivelinessScreen';
-import styles from './src/UI/Screens/Liveliness screens/styles';
+import { SafeAreaView, View } from 'react-native';
+import VisualElement from './src/UI/Screens/Frontend/VisualElementScreen';
+import styles from './src/UI/Screens/Frontend/styles';
+
 
 const App = () => {
   return (
+    <View style={{flex:1}}>
     <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      <LivelinessScreen />
+      <VisualElement
+        imageSource={require('./assets/imagenew.jpg')}
+        lottieSource={require('./assets/AnimationLottie.json')}
+        width={200}
+        height={200}
+        cornerRadius={20}
+        rotationAngle={0} text={'this is kubair app'}      />
     </SafeAreaView>
+    </View>
   );
 };
 
