4dc3700713bce4eca00a60b8985ed2cbb6d7fca0
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-07-01 11:52:20 +0530

    Created DebitCard.tsx to view card in CardsCarousel

--- src/UI/Components/CardsCarousel/CardsCarousel.tsx
+++ src/UI/Components/CardsCarousel/CardsCarousel.tsx
@@ -1,26 +1,25 @@
 import React, { useRef, useState } from 'react';
-import { View, FlatList, StyleSheet, Dimensions, Animated } from 'react-native';
+import { View, FlatList, Dimensions, Animated, NativeSyntheticEvent, NativeScrollEvent } from 'react-native';
 import Pagination from './Pagination';
-import style from './styles';
+import styles from './styles';
+import DebitCard from './DebitCard';
 
 const { width, height } = Dimensions.get('window');
-const styles = StyleSheet.create(style);
 
 type CardsCarouselProps = {
   data: any[];
-  renderItem: (item: any) => React.ReactElement<any, string | React.JSXElementConstructor<any>> | null;
 };
 
-const CardsCarousel = ({ data, renderItem }: CardsCarouselProps) => {
-  const flatListRef = useRef(null);
+const CardsCarousel = ({ data }: CardsCarouselProps) => {
+  const flatListRef = useRef<FlatList<any>>(null);
   const scrollX = useRef(new Animated.Value(0)).current;
   const [listData] = useState([...data, ...data, ...data]);
 
-  const handleScrollEnd = (event: any) => {
+  const handleScrollEnd = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
     const contentOffsetX = event.nativeEvent.contentOffset.x;
     const index = Math.round(contentOffsetX / width);
     if (index >= data.length && flatListRef.current !== null) {
-      (flatListRef.current as any).scrollToIndex({ index: index % data.length, animated: false });
+      flatListRef.current.scrollToIndex({ index: index % data.length, animated: false });
     }
   };
 
@@ -30,7 +29,11 @@ const CardsCarousel = ({ data, renderItem }: CardsCarouselProps) => {
         <FlatList
           ref={flatListRef}
           data={listData}
-          renderItem={renderItem}
+          renderItem={({ item }) => (
+            <View style={styles.cardContainer}>
+              <DebitCard item={item} />
+            </View>
+          )}
           keyExtractor={(item, index) => index.toString()}
           horizontal
           pagingEnabled
--- src/UI/Components/CardsCarousel/styles.tsx
+++ src/UI/Components/CardsCarousel/styles.tsx
@@ -1,23 +1,98 @@
-import {StyleSheet, Dimensions} from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    carouselContainer: {
-      height: height * 0.5,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    carousel: {
-      flexGrow: 0.55,
-    },
-  });
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carouselContainer: {
+    height: height * 0.7,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  carousel: {
+    flexGrow: 0.55,
+  },
+  cardContainer: {
+    width: width * 0.8,
+    marginHorizontal: width * 0.2 / 2,
+  },
+  debitCard: {
+    height: height * 0.28,
+    borderRadius: 10,
+    padding: 20,
+    justifyContent: 'space-between',
+    position: 'relative',
+    overflow: 'hidden',
+  },
+  debitCardBackground: {
+    borderRadius: 10,
+  },
+  chipImage: {
+    width: 50,
+    height: 40,
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  bankName: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  cardBalace: {
+    color: '#000',
+    marginTop: 12,
+    fontSize: 12,
+  },
+  cardNumber: {
+    color: '#fff',
+    fontSize: 25,
+    marginTop: 12,
+    fontWeight: 'bold',
+    letterSpacing: 2,
+  },
+  cardHolder: {
+    color: '#fff',
+    fontSize: 13,
+    marginBottom: -10,
+  },
+  balanceContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  balance: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    color: '#000',
+    fontSize: 18,
+    marginHorizontal: 10,
+    fontWeight: 'bold',
+  },
+  hideBalanceButton: {
+    marginLeft: 10,
+  },
+  balanceButtonImage: {
+    width: 20,
+    height: 20,
+  },
+  appContainer: {
+    flex: 1,
+    backgroundColor: '#333',
+  },
+  cardOwner: {
+    borderRadius: 10,
+    marginTop: 10,
+    padding: 15,
+    margin: -15,
+    backgroundColor: '#292628',
+  },
+});
 
 export default styles;
