793d38f441d810ae82dc03274058d3b3031e2d76
ChiragRai31 <namanabhinavrai@gmail.com>
2024-07-04 15:13:31 +0530

    Secure Component updated

--- index.js
+++ index.js
@@ -5,5 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import SecureComponent from './src/UI/Components/SecureComponent/index'
 import AadhaarConsentScreens from './src/UI/Screens/AadhaarConsentScreens/index';
-AppRegistry.registerComponent(appName, () => AadhaarConsentScreens);
+AppRegistry.registerComponent(appName, () => SecureComponent);
