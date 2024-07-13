f82f527048bd4b7a066a1dd7645de79d119a0983
Saurabh Kumar <saurabhk.ug22.ec@nitp.ac.in>
2024-07-13 16:10:22 +0530

    transactionItem type

---
+++ src/UI/Components/TransactionItem/type.ts
@@ -0,0 +1,11 @@
+/**
+ * Types for transaction item props.
+ */
+export type TransactionItemProps = {
+  type: string;
+  recipient: string;
+  amount: string;
+  date: string;
+  time: string;
+  status: string;
+};
