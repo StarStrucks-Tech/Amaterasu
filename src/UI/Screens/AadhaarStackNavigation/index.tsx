ce1e7ac84ab8acb01fb534d16ca39d762ccec511
ChiragRai31 <namanabhinavrai@gmail.com>
2024-07-18 18:25:08 +0530

    Constant file update

--- src/UI/Screens/AadhaarStackNavigation/index.tsx
+++ src/UI/Screens/AadhaarStackNavigation/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Aadhaardetails from '../AadhaarDetailScreen';
+//import Aadhaardetails from '../AadhaarDetailScreen';
 import AadhaarConsentScreens from '../AadhaarConsentScreens';
 //import LoadingScreen from './src/UI/Screens/LoadingScreen';
 //import AadhaarOTPScreen from './src/UI/Screens/AadhaarOTPScreen';
@@ -16,12 +16,12 @@ function AadhaarStackNavigation() {
               name="AadhaarConsentScreens"
               component={AadhaarConsentScreens}
               options={{ headerShown: false }}
-          />
+          />{/*
            <Stack.Screen
               name="Aadhaardetails"
               component={Aadhaardetails}
               options={{ headerShown: false }}
-          />{/*
+          />
           <Stack.Screen
               name="AadhaarOTPScreen"
               component={AadhaarOTPScreen}
