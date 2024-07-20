4ad8c422eb3a0f6dad1497adb0f770716a0d7571
Shivam Kumar Sinha <sksinha200476@gmail.com>
2024-07-20 19:49:09 +0530

    Created HomeScreen/QuickAction (#29)
    * Created QuickAction for HomeScreen
    
    * Added needed png
    
    * QuickAction
    
    * added missing file
    
    * Completed QuickAction
    
    * gencode commit (#32)
    
    Co-authored-by: kshitizjha <jhakshitiz911@gmail.com>
    
    * gencode commit (#35)
    
    Co-authored-by: kshitizjha <jhakshitiz911@gmail.com>
    
    * gencode commit (#36)
    
    * Added Documentation
    
    * Added vector icons dependancy
    
    * Used vector icon library instead of png images
    
    * Removed unused assets Images which are replaced by vector icons
    
    * Renamed files
    
    ---------
    
    Co-authored-by: kshitizjha <jhakshitiz911@gmail.com>

---
+++ src/UI/Components/QuickAction/style.tsx
@@ -0,0 +1,39 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  quickActionText: {
+    fontSize: 16,
+    color: '#000',
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  quickActionBar: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    paddingVertical: 10,
+    backgroundColor: '#FF00',
+    borderTopWidth: 1,
+    borderTopColor: '#ddd',
+  },
+
+  quickActionIcon: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 10,
+  },
+  actionButton: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  actionText: {
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+});
