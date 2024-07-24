afde06689895f7c1e6eb6154d997907c97daf2e1
Abhinav Anand <anandabhinav785@gmail.com>
2024-07-24 15:26:49 +0530

    moved styling files inside screen folder

--- App.tsx
+++ App.tsx
@@ -6,30 +6,94 @@
  */
 
 import React from 'react';
-import {SafeAreaView, StyleSheet, View} from 'react-native';
-import SplashScreen from './src/ui/screens/SplashScreen'
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import TabNavigation from './src/UI/Components/TabNavigation';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { OnboardingRpcCaller } from './src/Rpc/OnboardingRpcCaller';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
+
 function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
   // Enabling Text Encoder for safe RPC Calls
   global.TextEncoder = require('text-encoding').TextEncoder;
   
-  return (
-  <SafeAreaView style={styles.safeArea} >
-    <View style={styles.container}>
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
 
-    <SplashScreen/>
-    </View>
-  </SafeAreaView>  
-    );
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+    </SafeAreaView>
+  );
 }
+
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-    backgroundColor: 'white',
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
   },
 });
+
 export default App;
