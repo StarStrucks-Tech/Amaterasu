dfa17059011c2b498df6c32017f2b6e1b901a9a5
Arpita Dwivedi <arpitad.ug22.cs@nitp.ac.in>
2024-07-25 18:54:50 +0530

    updated with develop

--- App.tsx
+++ App.tsx
@@ -1,50 +1,31 @@
 
-// import React from 'react';
-// import {SafeAreaView, StyleSheet, View} from 'react-native';
-// import SplashScreen from './src/ui/screens/SplashScreen'
-// function App(): React.JSX.Element {
-//   // Enabling Text Encoder for safe RPC Calls
-//   global.TextEncoder = require('text-encoding').TextEncoder;
-  
-//   return (
-//   <SafeAreaView style={styles.safeArea} >
-//     <View style={styles.container}>
-
-//     <SplashScreen/>
-//     </View>
-//   </SafeAreaView>  
-//     );
-// }
-// const styles = StyleSheet.create({
-//   safeArea: {
-//     flex: 1,
-//     backgroundColor: 'white',
-//   },
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-// });
-// export default App;
-
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text } from 'react-native';
-import PrevRecipient from './src/UI/Components/PrevRecipients/index';
-
-const App = () => {
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import SplashScreen from './src/ui/screens/SplashScreen'
+function App(): React.JSX.Element {
+  // Enabling Text Encoder for safe RPC Calls
+  global.TextEncoder = require('text-encoding').TextEncoder;
+  
   return (
-    <SafeAreaView style={styles.container}>
-      <PrevRecipient/>
-    </SafeAreaView>
-  );
-};
+  <SafeAreaView style={styles.safeArea} >
+    <View style={styles.container}>
 
+    <SplashScreen/>
+    </View>
+  </SafeAreaView>  
+    );
+}
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#FFFFE4',
-    paddingTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 export default App;
+
+
